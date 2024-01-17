94c364a632420a30b3e9738b0f75b89a9463fa15
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-17 12:08:24 +0530

    Update AuthContext.js

---
+++ contexts/AuthContext.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // initialize as false initially
+
+    useEffect(() => {
+        // This will run client-side after initial render
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token);
+    }, []);
+
+    const login = (token) => {
+        localStorage.setItem('token', token);
+        setIsLoggedIn(true);
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
